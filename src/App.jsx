d47c09ff228f0be6a954c09660bce39aac4f14fa
Bruce <nbruce420@gmail.com>
2023-03-21 21:48:51 +0200

    bruce travels

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="App">
+            <h4 className="flex items-center bg-emerald-700 font-bold text-4xl text-white text-center">
+                Bruce Travels
+            </h4>
+        </div>
+    );
+}
+
+export default App;
